cf6549a9c260d1f1027a01b9d769ce7033d6b1cf
Gashaw Kidanu <kidanugashaw@gmail.com>
2022-12-11 14:58:21 +0300

    App structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "./style.css";
+import Home from "./components/Home/Home";
+import NavBar from "./components/Navbar";
+import Project from "./components/Projects";
+import About from "./components/About";
+import Resume from "./components/Resume";
+import Footer from "./components/footer";
+import ScrollToTop from "./components/ScrollToTop";
 
-import './App.css';
-import Home from './components/Home/Home';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [load, upadateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
-    <div className="App">
-          <Home/>
-    </div>
+    <Router>
+      <div className="App-header" id={load ? "no-scroll" : "scroll"}>
+        <NavBar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 

a9b893af3b88c32e24c6f140b3b1aaf755898257
Gashaw Kidanu <kidanugashaw@gmail.com>
2022-12-23 15:20:59 +0300

    css for contact header applied

--- src/components/contact/ContactHeader.js
+++ src/components/contact/ContactHeader.js
@@ -3,15 +3,17 @@ import { AiOutlineMail } from "react-icons/ai";
 const ContactHeader = () => {
   return (
     <>
-      <h1 className="contact_title">GET IN TOUCH</h1>
-      <h6>
+      <h1 className="contact_title">GET IN TOUCH </h1>
+      <div className="main-contact">
+      <h6 >
         {" "}
         <FaPhone /> Phone:<span> +251 941 623 091</span>
       </h6>
-      <h6>
+      <h6 style={{paddingLeft:'60px'}}>
         <AiOutlineMail /> Email:{" "}
         <span className="mailContent">kidanugashaw@gmail.com</span>
       </h6>
+      </div>
     </>
   );
 };
